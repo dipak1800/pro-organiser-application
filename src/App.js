ca5bb4f945419ba5f8d8dda9bc73d3e9ac9b0567
Dipak Roy <roydeepak1800@gmail.com>
2020-07-08 16:49:58 +0530

    added signin feature again
    Test cases passed

--- src/App.js
+++ src/App.js
@@ -1,49 +1,64 @@
 import React from "react";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { Route, Switch,/* Redirect*/ } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./Components/NavBar/NavBar";
 import Homepage from "./Pages/Homepage/Homepage";
 import Board_Page from "./Pages/Create_Board_Page/Board_Page";
 import MyBoard_Page from "./Pages/Individual_Board_Page/MyBoard";
-// import SignIn_SignUp_Page from "./Pages/SignIn_SignUp _Page/SignIn_SignUp";
-// import { auth, createUserProfileDocument } from "./Firebase/firebase.utils"; //so that our appn listens to user state changes from firebase
+import SignIn_SignUp_Page from "./Pages/SignIn_SignUp _Page/SignIn_SignUp";
+import { auth, createUserProfileDocument } from "./Firebase/firebase.utils"; //so that our appn listens to user state changes from firebase
 
 const App = () => {
-  // const [currentUser, setCurrentUser] = useState(null);
-  // let unSubscribeFromAuth = null;
-  // useEffect(() => {
-  //   unSubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-  //     // setCurrentUser(user);
-  //     // createUserProfileDocument(user);
-  //     // console.log(user);
-  //     if (userAuth) {
-  //       const userRef = await createUserProfileDocument(userAuth);
-  //       userRef.onSnapshot((snapShot) => {
-  //         // console.log(snapShot.data());
-  //         setCurrentUser({ id: snapShot.id, ...snapShot.data() });
-  //       });
-  //     } else {
-  //       setCurrentUser(userAuth);
-  //     }
-  //   });
-  //   return () => {
-  //     console.log("it works as component will unmount.");
-  //     unSubscribeFromAuth(); //it will close the subscription
-  //   };
-  // }, []);
-  // console.log(currentUser);
+  const [currentUser, setCurrentUser] = useState(null);
+  let unSubscribeFromAuth = null;
+  useEffect(() => {
+    unSubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      // setCurrentUser(user);
+      // createUserProfileDocument(user);
+      // console.log(user);
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          // console.log(snapShot.data());
+          setCurrentUser({ id: snapShot.id, ...snapShot.data() });
+        });
+      } else {
+        setCurrentUser(userAuth);
+      }
+    });
+    return () => {
+      console.log("it works as component will unmount.");
+      unSubscribeFromAuth(); //it will close the subscription
+    };
+  }, []);
+  console.log(currentUser);
   return (
     <div className="App">
-      <NavBar /*currentUser={currentUser} *//>
+      <NavBar currentUser={currentUser} />
       <Switch>
-        {/* <Route path="/signin" component={SignIn_SignUp_Page} /> */}
-        {/* <Route path="/" exact render={()=>currentUser?<Homepage/>:<Redirect to="/signin"/>} />
-        <Route path="/createboard" exact strict render={()=>currentUser?<Board_Page/>:<Redirect to="/signin"/>} />
-        <Route path="/:boardName" render={()=>currentUser?<MyBoard_Page/>:<Redirect to="/signin"/>}/> */}
-        <Route path="/" exact component={Homepage} />
-        <Route path="/createboard" exact strict component={Board_Page} />
-        <Route path="/:boardName" component={MyBoard_Page} />
+        <Route path="/signin" component={SignIn_SignUp_Page} />
+        <Route
+          path="/"
+          exact
+          render={() =>
+            currentUser ? <Homepage /> : <Redirect to="/signin" />
+          }
+        />
+        <Route
+          path="/createboard"
+          exact
+          strict
+          render={() =>
+            currentUser ? <Board_Page /> : <Redirect to="/signin" />
+          }
+        />
+        <Route
+          path="/:boardName"
+          render={() =>
+            currentUser ? <MyBoard_Page /> : <Redirect to="/signin" />
+          }
+        />
       </Switch>
     </div>
   );
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 import Style from "./NavBar.module.scss";
 import { NavLink } from "react-router-dom";
-// import { auth } from "../../Firebase/firebase.utils";
-// import swal from "sweetalert";
+import { auth } from "../../Firebase/firebase.utils";
+import swal from "sweetalert";
 const NavBar = ({ currentUser }) => {
-  // let handleSignOut = () => {
-  //   swal({
-  //     title: "Are you sure?",
-  //     text: " Do you Want to Sign Out??",
-  //     icon: "warning",
-  //     buttons: ["Oh noez!", "yup!"],
-  //     dangerMode: true,
-  //   }).then((willDelete) => {
-  //     if (willDelete) {
-  //       auth.signOut();
-  //     } else {
-  //       return;
-  //     }
-  //   });
-  // };
+  let handleSignOut = () => {
+    swal({
+      title: "Are you sure?",
+      text: " Do you Want to Sign Out??",
+      icon: "warning",
+      buttons: ["Oh noez!", "yup!"],
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        auth.signOut();
+      } else {
+        return;
+      }
+    });
+  };
   return (
     <div className={Style.container}>
       <h2 className={Style.brand}>
@@ -35,30 +35,39 @@ const NavBar = ({ currentUser }) => {
           activeClassName={Style.active}
           style={{ textDecoration: "none" }}
         >
-          <li className={Style.item1}>Home</li>
-
-          {/* <li  onClick={()=>{!currentUser && swal("You haven't Signed In!", "Please Sign In with registered Email or with Google",'info')}} className={Style.item1}>Home</li> */}
+          <li
+            onClick={() => {
+              !currentUser &&
+                swal(
+                  "You haven't Signed In!",
+                  "Please Sign In with registered Email or with Google",
+                  "info"
+                );
+            }}
+            className={Style.item1}
+          >
+            Home
+          </li>
         </NavLink>
         <NavLink
           to="/createboard"
           activeClassName={Style.active}
           style={{ textDecoration: "none" }}
         >
-          <li>Create a board</li>
-          {/* <li
-            // onClick={() => {
-            //   !currentUser &&
-            //     swal(
-            //       "You haven't Signed In!",
-            //       "Please Sign In with registered Email or with Google",
-            //       "info"
-            //     );
-            // }}
+          <li
+            onClick={() => {
+              !currentUser &&
+                swal(
+                  "You haven't Signed In!",
+                  "Please Sign In with registered Email or with Google",
+                  "info"
+                );
+            }}
           >
             Create a board
-          </li> */}
+          </li>
         </NavLink>
-        {/* {currentUser ? (
+        {currentUser ? (
           <li onClick={handleSignOut}>Sign Out</li>
         ) : (
           <NavLink
@@ -69,7 +78,7 @@ const NavBar = ({ currentUser }) => {
           >
             <li className={Style.item1}>Sign In</li>
           </NavLink>
-        )} */}
+        )}
       </ul>
     </div>
   );
